4a04e2c4139b694534ccd0321fcd1b3a7131b0b9
Dmitrii Semenov <124681154+dmitrii-semenov@users.noreply.github.com>
2024-04-18 14:25:07 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,1 +1,58 @@
+import machine
+import time
+import BG77
 
+_REMOTE_SERVER_IP_ = "147.229.146.40"
+_REMOTE_SERVER_PORT_ = 1883
+_CLIENT_ID_ = "ac24c3a0-f904-11ee-b0f6-299dee8b9dbf"
+_ACCESS_TOKEN_ = "052du7z8d2xagimrfdsh"
+_PASSWORD_ = ""
+
+_REMOTE_SERVER_IP_PROXY_ = ""
+_REMOTE_SERVER_PORT_PROXY_ = 7004
+
+coordinates = [
+    (49.218319, 16.581795),
+    (49.224934, 16.599390),
+    (49.202283, 16.619560),
+    (49.204638, 16.571667),
+]
+
+# Initialize UART for BG77 module
+bg_uart = machine.UART(0, baudrate=115200, tx=machine.Pin(0), rxbuf=256, rx=machine.Pin(1), timeout=0, timeout_char=1)
+bg_uart.write(bytes("AT\r\n", "ascii"))
+
+# Initialize BG77 module
+module = BG77.BG77(bg_uart, verbose=True, radio=False)
+time.sleep(3)
+
+# Set radio mode and APN configuration
+module.setRadio(1)
+time.sleep(1)
+module.setAPN("lpwa.vodafone.iot")
+time.sleep(1)
+module.attachToNetwork()
+time.sleep(5)
+
+# Open socket
+module.sendCommand(f"AT+QIOPEN=1,1,\"UDP\",\"{_REMOTE_SERVER_IP_PROXY_}\",{_REMOTE_SERVER_PORT_PROXY_}\r\n")
+
+# Check network registration status using AT+CEREG?
+cereg_response = module.sendCommand("AT+CEREG?\r\n")
+time.sleep(1)
+print("Network Registration Status (CEREG):", cereg_response)
+
+while True:
+# WAKE UP
+
+    # Send message
+    latitude, longitude = coordinates[1]
+    gps_data = f"{{\"latitude\":{latitude},\"longitude\":{longitude}}}"
+    module.sendCommand(f"AT+QISEND=1,{len(gps_data)}\r\n")
+    time.sleep(1)
+    module.sendCommand(gps_data + "\r\n")
+# Check downlink message
+
+# Sleep mode activation
+
+    time.sleep(n)
